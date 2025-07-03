51af0b23169bbc1db915350dc78ec74afe4cc5a9
frederikciupek <131395997+frederikciupek@users.noreply.github.com>
2025-07-03 15:29:37 +0000

    Deploying to gh-pages from @ frederikciupek/frederikciupek.github.io@abc96f3e4add1bb3e10620d5bfd23cf2cbe794a1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -91,6 +91,17 @@ ninja.data = [{
             window.location.href = "/blog/2025/non-farm-may2025/";
           
         },
+      },{id: "post-labor-market-seems-strong-but-with-some-caveats",
+        
+          title: "Labor market seems strong - but with some caveats",
+        
+        description: "The labor market appears surprisingly resilient with 137k jobs added - beating expectations. But not all is as good as it seems",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/labor-market-june2025/";
+          
+        },
       },{id: "post-ada-employment-report-points-to-strongly-weakening-labour-market",
         
           title: "ADA Employment Report points to strongly weakening Labour market",
